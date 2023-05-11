656d7b5b2a7735dc8fc7321852c4d2d8e2d4515b
daianne dorigatti <daiannedorigatti@gmail.com>
2023-05-11 16:13:44 -0300

    adicionando requisito 1

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,27 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
